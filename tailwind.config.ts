dad2c0722ccb40be9b9ca2ce62904cd01f58f22c
William Phan <willdphan@gmail.com>
2024-10-11 01:48:40 -0700

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
